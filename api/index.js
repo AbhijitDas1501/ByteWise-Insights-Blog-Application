bac693086c1673c5f1c4d6228a7940e81d4cb696
Abhijit Das <abhijitdas.munnu@gmail.com>
2024-04-30 15:49:21 +0530

    add a middleware and a function to handle error

--- api/index.js
+++ api/index.js
@@ -17,6 +17,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express();
+
+
 app.use(express.json());
 
 
@@ -27,3 +29,16 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+
+app.use((err, req, res, next) =>{
+   const statusCode = err.statusCode || 500;
+   const message = err.message || "Internal Server Error";
+
+   res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message
+   })
+   
+});
